5b8c2875975d170e4ec00357969bb1ec005e39a1
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-07-15 23:30:06 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-3xl bg-zinc-600 text-white'>Hello i am tailwind</h1>
+      <button className="btn btn-neutral">Neutral</button>
+    </div>
+  )
+}
+
+export default App
