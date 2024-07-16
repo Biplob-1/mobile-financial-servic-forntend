5c093814111bd4cb5e73ec953701676d0b1bb58f
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-07-16 23:42:12 +0600

    sidebar menu update by user roll

--- src/Pages/Dashboard/Sidebar/Sidebar.jsx
+++ src/Pages/Dashboard/Sidebar/Sidebar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import LogOut from '../LogOut/LogOut';
+import { AuthContext } from '../../../Providers/AuthProvider';
 
 const Sidebar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="flex h-screen">
       <div className="flex flex-col w-32 md:w-64 h-full bg-gray-800 text-white shadow-lg">
@@ -11,38 +14,52 @@ const Sidebar = () => {
         </div>
         <div className="flex-grow">
           <nav className="flex flex-col mt-4">
-            {/* admin path */}
-            <Link to="user-management" className="flex items-center px-4 py-2 hover:bg-gray-700">
-              User Management
-            </Link>
-            <Link to="system-monitoring" className="flex items-center px-4 py-2 hover:bg-gray-700">
-            System Monitoring
-            </Link>
-            {/* agent path */}
-            <Link to="transaction-management" className="flex items-center px-4 py-2 hover:bg-gray-700">
-            Transaction Management
-            </Link>
-            {/* user path */}
-            <Link to="send-money" className="flex items-center px-4 py-2 hover:bg-gray-700">
-              Send Money
-            </Link>
-            <Link to="cash-out" className="flex items-center px-4 py-2 hover:bg-gray-700">
-              Cash-Out
-            </Link>
-            <Link to="cash-in" className="flex items-center px-4 py-2 hover:bg-gray-700">
-              Cash-In
-            </Link>
-            {/* common path */}
-            <Link to="balance-inquiry" className="flex items-center px-4 py-2 hover:bg-gray-700">
-              Balance Inquiry
-            </Link>
-            <Link to="transactions-history" className="flex items-center px-4 py-2 hover:bg-gray-700">
-              Transactions History
-            </Link>
-            {/* <Link to="logout" className="flex items-center px-4 py-2 hover:bg-gray-700">
-              Logout
-            </Link> */}
-            <LogOut />
+            {/* admin paths */}
+            {user && user.role === 'admin' && (
+              <>
+                <Link to="user-management" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                  User Management
+                </Link>
+                <Link to="system-monitoring" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                  System Monitoring
+                </Link>
+              </>
+            )}
+
+            {/* agent paths */}
+            {user && user.role === 'agent' && (
+              <Link to="transaction-management" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                Transaction Management
+              </Link>
+            )}
+
+            {/* user paths */}
+            {user && user.role === 'user' && (
+              <>
+                <Link to="send-money" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                  Send Money
+                </Link>
+                <Link to="cash-out" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                  Cash-Out
+                </Link>
+                <Link to="cash-in" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                  Cash-In
+                </Link>
+              </>
+            )}
+
+            {/* Common paths for all roles */}
+            {user && (
+              <>
+                <Link to="balance-inquiry" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                  Balance Inquiry
+                </Link>
+                <Link to="transactions-history" className="flex items-center px-4 py-2 hover:bg-gray-700">
+                  Transactions History
+                </Link>
+                <LogOut />
+              </>
+            )}
           </nav>
         </div>
       </div>
